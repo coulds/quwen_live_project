066046d284678ddf53808586bf109b0bec263ea6
coulds <1193618579@qq.com>
2020-12-31 17:15:42 +0800

    完成去问，手机号的页面。

---
+++ app/src/main/java/com/hsjskj/quwen/ui/activity/AboutQuWenActivity.java
@@ -0,0 +1,26 @@
+package com.hsjskj.quwen.ui.activity;
+
+import com.hsjskj.quwen.R;
+import com.hsjskj.quwen.common.MyActivity;
+
+/**
+ * @author : sen
+ * time          : 2020年12月31日 16:10
+ * description   : quwen_live
+ */
+public class AboutQuWenActivity extends MyActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.about_quwen_activity;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
---
+++ app/src/main/java/com/hsjskj/quwen/ui/activity/SetPhoneCodeActivity.java
@@ -0,0 +1,26 @@
+package com.hsjskj.quwen.ui.activity;
+
+import com.hsjskj.quwen.R;
+import com.hsjskj.quwen.common.MyActivity;
+
+/**
+ * @author : sen
+ * time          : 2020年12月31日 16:51
+ * description   : quwen_live
+ */
+public class SetPhoneCodeActivity extends MyActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.set_phonecode_activity;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
