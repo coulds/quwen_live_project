cdb2777ae1a9e347c222568a95ef6bb0f109ab1a
coulds <1193618579@qq.com>
2020-12-31 17:34:09 +0800

    完成发送邮箱的页面,同时增加遗忘的手机号页面注册.

---
+++ app/src/main/java/com/hsjskj/quwen/ui/activity/EmailCodeActivity.java
@@ -0,0 +1,26 @@
+package com.hsjskj.quwen.ui.activity;
+
+import com.hsjskj.quwen.R;
+import com.hsjskj.quwen.common.MyActivity;
+
+/**
+ * @author : sen
+ * time          : 2020年12月31日 17:21
+ * description   : quwen_live
+ */
+public class EmailCodeActivity extends MyActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.email_code_activity;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
