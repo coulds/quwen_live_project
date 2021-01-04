1a8fb7e24311939a91d5006940d2e13aff01ff35
coulds <1193618579@qq.com>
2021-01-04 10:56:37 +0800

    增加修改密码页面

---
+++ app/src/main/java/com/hsjskj/quwen/ui/activity/ProblemFeedBackActivity.java
@@ -0,0 +1,26 @@
+package com.hsjskj.quwen.ui.activity;
+
+import com.hsjskj.quwen.R;
+import com.hsjskj.quwen.common.MyActivity;
+
+/**
+ * @author : sen
+ * time          : 2021年01月04日 09:51
+ * description   : quwen_live
+ */
+public class ProblemFeedBackActivity extends MyActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.set_problem_feedback_activity;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
---
+++ app/src/main/java/com/hsjskj/quwen/ui/activity/SetPassWordActivity.java
@@ -0,0 +1,26 @@
+package com.hsjskj.quwen.ui.activity;
+
+import com.hsjskj.quwen.R;
+import com.hsjskj.quwen.common.MyActivity;
+
+/**
+ * @author : sen
+ * time          : 2021年01月04日 09:21
+ * description   : quwen_live
+ */
+public class SetPassWordActivity extends MyActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.set_password_activity;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
